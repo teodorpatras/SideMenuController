1abdc72c7cd35fd6c5872a3def0fd9b1a7268328
Sergey Navka <sergeynavka@macmini001s-Mac-mini.local>
2017-01-16 15:57:05 +0200

    add ability to resize menu to full scrren

--- Example/Example/MenuController.swift
+++ Example/Example/MenuController.swift
@@ -12,17 +12,19 @@ class MenuController: UITableViewController {
     
     let segues = ["showCenterController1", "showCenterController2", "showCenterController3"]
     private var previousIndex: NSIndexPath?
+    private var searchController: UISearchController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureSearchController()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return segues.count
     }
     
-        override func tableView(_ tableView: UITableView,
-                                cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "menuCell")!
         cell.textLabel?.font = UIFont(name: "HelveticaNeue-Light", size: 15)
         cell.textLabel?.text = "Switch to controller \(indexPath.row + 1)"
@@ -30,8 +32,8 @@ class MenuController: UITableViewController {
         return cell
     }
     
-        override func tableView(_ tableView: UITableView,
-                                didSelectRowAt indexPath: IndexPath)  {
+    override func tableView(_ tableView: UITableView,
+                            didSelectRowAt indexPath: IndexPath)  {
         
         if let index = previousIndex {
             tableView.deselectRow(at: index as IndexPath, animated: true)
@@ -41,4 +43,21 @@ class MenuController: UITableViewController {
         previousIndex = indexPath as NSIndexPath?
     }
     
+    private func configureSearchController() {
+        searchController = UISearchController(searchResultsController: UITableViewController())
+        searchController.dimsBackgroundDuringPresentation = true;
+        searchController.delegate = self
+        tableView.tableHeaderView = searchController.searchBar
+    }
+}
+
+extension MenuController: UISearchControllerDelegate {
+    
+    func willPresentSearchController(_ searchController: UISearchController) {
+        sideMenuController?.sideMenu(toFull: true)
+        
+    }
+    func didDismissSearchController(_ searchController: UISearchController) {
+        sideMenuController?.sideMenu(toFull: false)
+    }
 }
