cdd96e304d94b85377679b76ba5a8264a7241c53
Bjarke Hesthaven Søndergaard <bhsgaard@gmail.com>
2017-02-24 12:56:20 +0100

    Disabling opening the side menu
    Sometimes it would be nice to be able to disable opening the side menu even while there is a view controller. I'm not entirely sure about the naming of the property.
    
    My use case is that I have a subview of my navigation controller where I want to disable the side menu. I don't want to completely remove it just for this one view.

--- Source/SideMenuController.swift
+++ Source/SideMenuController.swift
@@ -406,7 +406,10 @@ open class SideMenuController: UIViewController, UIGestureRecognizerDelegate {
         return screenSize.width < screenSize.height
     }
     
+    open var sidePanelDisabled = false
+    
     var canDisplaySideController: Bool {
+        guard !sidePanelDisabled else { return false }
         return sideViewController != nil
     }
     
