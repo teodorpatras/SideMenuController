90bb12bb8fd1841a53f78a6b8ec5eb159f1b985b
Marc <marc.garcia@sundiogroup.com>
2018-04-12 15:50:19 +0200

    Add parameter to embed method to indicate whether the menu should close after embedding the centerViewController

--- Source/SideMenuController.swift
+++ Source/SideMenuController.swift
@@ -87,7 +87,7 @@ public extension SideMenuController {
      - parameter centerViewController: controller to be embedded
      - parameter cacheIdentifier: identifier for the view controllers cache
      */
-    public func embed(centerViewController controller: UIViewController, cacheIdentifier: String? = nil) {
+    public func embed(centerViewController controller: UIViewController, cacheIdentifier: String? = nil, closeMenu: Bool = true) {
         
         guard controller !== centerViewController else {
             if sidePanelVisible {
@@ -126,7 +126,7 @@ public extension SideMenuController {
                 completion()
             }
             
-            if sidePanelVisible {
+            if sidePanelVisible && closeMenu {
                 animate(toReveal: false)
             }
         }
